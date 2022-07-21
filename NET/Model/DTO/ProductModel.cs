f004d782f925c5555e4365ea7a19071b4933e378
Dawid Kacprzak <dawidkacprzak@Dawids-Mac-mini.local>
2022-07-21 11:03:13 +0200

    Scroll products

---
+++ NET/Model/DTO/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace honeyshop.web.Model.DTO
+{
+    public class ProductModel
+    {
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Price { get; set; }
+        public long ProductId { get; set; }
+    }
+}
+
